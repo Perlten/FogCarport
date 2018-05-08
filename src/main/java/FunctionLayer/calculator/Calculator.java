49b12d05dc46511d7986fd4dcf8b6e622eb5176b
adamlass <33089759+adamlass@users.noreply.github.com>
2018-05-08 11:24:08 +0200

    cladding and door

--- src/main/java/FunctionLayer/calculator/Calculator.java
+++ src/main/java/FunctionLayer/calculator/Calculator.java
@@ -6,6 +6,7 @@
 package FunctionLayer.calculator;
 
 import FunctionLayer.entities.Customization;
+import FunctionLayer.entities.Shed;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,16 +17,23 @@ import java.util.List;
 public class Calculator {
 
     private Customization cust;
+    private Shed shed;
+
     private List<Product> products;
     private final int rafterWoodLength = 600; //TODO make db
+    private final int maxHeight = 480;
 
     public Calculator(Customization cust) {
         this.products = new ArrayList<>();
         this.cust = cust;
+        this.shed = cust.getShed();
     }
 
     public void calculate() {
         calculateRafter();
+        if(shed != null){
+          calculateCladding();
+        }
     }
 
     public List<Product> getProducts() {
@@ -42,7 +50,7 @@ public class Calculator {
 
         int pitstops = (int) (remainder - 1);
 
-            polesAndBeams(pitstops);
+        polesAndBeams(pitstops);
 
         amountOfRafters *= remainder;
 
@@ -57,7 +65,7 @@ public class Calculator {
         int lanes = 2 + pitstops;
         int placingLength = cust.getLength() - Customization.padding;
 
-        double amountOfBeams = ((double) placingLength)/ rafterWoodLength;
+        double amountOfBeams = ((double) placingLength) / rafterWoodLength;
 
         if (amountOfBeams < 1) {
             amountOfBeams = 1;
@@ -77,15 +85,25 @@ public class Calculator {
 
         poles += placingLength / (rafterWoodLength / 2);
 
-        if ((placingLength / ((double)(rafterWoodLength)) / 2) % 1 != 0) {
+        if ((placingLength / ((double) (rafterWoodLength)) / 2) % 1 != 0) {
             poles++;
         }
 
         products.add(new Product("Pole", "Used to support the beams", "pcs", poles * lanes, cust.getHeight(), 0)); //TODO make db
     }
-    
+
+    private void calculateCladding() {
+        int circumsference = shed.getLength() + shed.getWidth() * 2;
+        int claddingNeeded = (int) Math.ceil(circumsference * 0.125);
+        int claddingHeight = cust.getHeight() - 20; //20 cm air from floor
+        products.add(new Product("Cladding", "Cladding used og the shed", "pcs", claddingNeeded, claddingHeight, 0)); //TODO makedb
+        
+    }
+
     public static void main(String[] args) {
-        Customization cust = new Customization(700, 700, 200, 0, null, null, null);
+        Shed shed = new Shed(200, 640);
+        shed = null;
+        Customization cust = new Customization(700, 700, 200, 0, shed, null, null);
         Calculator cal = new Calculator(cust);
         cal.calculate();
         System.out.println(cal.getProducts());

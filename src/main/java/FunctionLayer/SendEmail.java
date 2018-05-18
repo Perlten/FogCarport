a09f325029766b2f85fa87251da12c852b2be17f
Perlt <nikolaiperlt@gmail.com>
2018-05-18 14:12:50 +0200

    Optimized email to only log in once

--- src/main/java/FunctionLayer/SendEmail.java
+++ src/main/java/FunctionLayer/SendEmail.java
@@ -16,29 +16,21 @@ import javax.mail.internet.*;
  */
 public class SendEmail {
 
+    private static Session session = null;
+    private final static String USERNAME = "fogcarport@gmail.com";
+    private final static String PASSWORD = "FoggyFrog420";
+
     public static void sendMail(String mailTo, String title, String textMessage) throws FOGException {
 
-        final String username = "fogcarport@gmail.com";
-        final String password = "FoggyFrog420";
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+        if(session == null){
+            makeSession();
+        }
+        
         try {
-
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
+            message.setFrom(new InternetAddress(USERNAME));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailTo));
-            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(username));
+            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(USERNAME));
             message.setSubject(title);
             message.setText(textMessage);
             Transport.send(message);
@@ -46,5 +38,24 @@ public class SendEmail {
         } catch (MessagingException e) {
             throw new FOGException("Could not send email");
         }
+    }
+
+    private static void makeSession() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, PASSWORD);
+            }
+        });
+    }
+    
+    public static void main(String[] args) throws FOGException {
+        sendMail("karron11@gmail.com", "Test", "test");
     }
 }
